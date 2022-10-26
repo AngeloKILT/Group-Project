37f8d27faf067f1f656c3e03b499519623395a3b
AngeloKILT <115586068+AngeloKILT@users.noreply.github.com>
2022-10-26 15:20:38 -0500

    Add files via upload
    I'm a bit stuck

---
+++ PROGRAM TEAM.py
@@ -0,0 +1,16 @@
+print("A1 to learn about A")
+print("B2 to learn about B")
+print("C3 to learn about C")
+
+
+A1 = input("Please input letter that corresponds with its number")
+if A1:
+    print("This is about the letter A being the first letter  in the alphabet")
+
+B2 = input("press B2 for more information")
+if B2:
+    print("This is about the letter B the second letter in the alphabet")
+
+C3 = input("Please input letter that corresponds with its number")
+if C3:
+    print("This is about the letter C its the third letter in the alphabet")
